10af646731535a5a614d7a38e70d7cbaeca8c015
Unknown <winnery93@gmail.com>
2019-05-25 16:58:43 +0900

    utils 추가

---
+++ comicApi14/utils/responseMessage.js
@@ -0,0 +1,27 @@
+module.exports =  {
+    NULL_VALUE: "필요한 값이 없습니다.",
+    OUT_OF_VALUE: "파라미터 값이 잘못 되었습니다.",
+    ID_OR_PW_NULL_VALUE: "아이디/비밀번호 값이 없습니다.",
+    CREATED_USER: "회원 가입 성공",
+    CREATED_USER_FAIL: "회원 가입 성공",
+    DELETE_USER: "회원 탈퇴 성공",
+
+    ALREADY_USER: "이미 회원입니다.",
+    NO_USER: "존재하지 않는 회원입니다.",
+    MISS_MATCH_PW: "비밀번호가 맞지 않습니다.",
+    LOGIN_SUCCESS: "로그인 성공",
+    LOGIN_FAIL: "로그인 실패",
+    
+    ALREADY_BOARD: "이미 존재하는 게시글입니다",
+    CREATED_BOARD: "게시글 작성 성공",
+    UPDATED_BOARD: "게시글 수정 성공",
+    REMOVED_BOARD: "게시글 삭제 성공",
+    REMOVED_BOARD_FAIL: "게시글 삭제 실패",
+    NO_BOARD: "존재하지 않는 게시글입니다.",
+
+
+    FAIL_ENCRYPTION: "암호화에 실패했습니다.",
+
+    FAIL_CSV_READ: "csv 읽기 실패",
+    FAIL_CSV_WRITE: "csv 쓰기 실패",
+}
